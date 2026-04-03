71922b0cccfa4c44656125ce916ecc1a9f1aff5b
liberty <wkdghcjf1234@gmail.com>
2026-04-03 23:32:54 +0900

    AssetReferenceComponent 추가

---
+++ Script/Editor/Addressables/AssetReferenceComponentPropertyDrawer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Ayla;
+
+[CustomPropertyDrawer(typeof(AssetReferenceComponent<>))]
+internal class AssetReferenceComponentPropertyDrawer : PropertyDrawer
+{
+    private SerializedProperty? m_CachedProperty;
+    private Type? m_CachedComponentType;
+    private string? m_AssetGUID;
+    private GameObject? m_EditorAsset;
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUIUtility.singleLineHeight;
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        TryCacheProperty(property);
+
+        if (m_CachedComponentType == null)
+        {
+            EditorGUI.LabelField(position, label.text, "Invalid AssetReferenceComponent");
+            return;
+        }
+
+        var assetGUIDProperty = property.FindPropertyRelative("m_AssetGUID");
+        var assetGUID = assetGUIDProperty.stringValue;
+        if (m_AssetGUID != assetGUID)
+        {
+            m_AssetGUID = assetGUID;
+            if (GUID.TryParse(assetGUID, out var g))
+            {
+                m_EditorAsset = AssetDatabase.LoadAssetByGUID<GameObject>(g);
+            }
+            else
+            {
+                m_EditorAsset = null;
+            }
+        }
+
+        Component? currentComponent = m_EditorAsset ? m_EditorAsset.GetComponent(m_CachedComponentType) : null;
+        var newComponent = (Component?)EditorGUI.ObjectField(position, label, currentComponent, m_CachedComponentType, false);
+        if (GUI.changed)
+        {
+            var newGameObject = newComponent ? newComponent.gameObject : null;
+            if (newGameObject != m_EditorAsset)
+            {
+                var argo = (AssetReferenceGameObject)property.boxedValue;
+                argo.SetEditorAsset(newGameObject!);
+                assetGUIDProperty.stringValue = argo.AssetGUID;
+                m_EditorAsset = newGameObject;
+            }
+        }
+    }
+
+    private void TryCacheProperty(SerializedProperty property)
+    {
+        if (m_CachedProperty == property)
+        {
+            return;
+        }
+
+        m_CachedProperty = property;
+        if (property == null)
+        {
+            m_CachedComponentType = null;
+        }
+        else
+        {
+            var boxedValue = property.boxedValue;
+            var referenceType = boxedValue.GetType();
+            m_CachedComponentType = referenceType.GetGenericArguments()[0];
+        }
+    }
+}
---
+++ Script/Runtime/Addressables/AssetReferenceComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Ayla;
+
+[Serializable]
+public class AssetReferenceComponent<T> : AssetReferenceGameObject
+    where T : Component
+{
+    public AssetReferenceComponent(string guid) : base(guid)
+    {
+    }
+}
