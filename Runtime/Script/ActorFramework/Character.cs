bf831e8884e7c284794c8191a1419f352b793841
liberty <wkdghcjf1234@gmail.com>
2026-06-17 22:54:27 +0900

    Codex: Remove entity support from actor framework
    - Remove Entity, Physics, Transform, Burst, and Mathematics wiring from ActorFramework runtime types.
    - Delete the EntityWorldHelper utility and empty DOTS folder metadata.
    - Keep Character and CharacterMovement as plain Unity behaviours for future expansion.

--- Runtime/Script/ActorFramework/Character.cs
+++ Runtime/Script/ActorFramework/Character.cs
@@ -1,73 +1,8 @@
-﻿using Unity.Burst;
-using Unity.Collections;
-using Unity.Entities;
-using Unity.Mathematics;
-using Unity.Physics;
-using Unity.Transforms;
-
 #nullable enable
 
 namespace Ayla
 {
-    [BurstCompile]
     public class Character : Actor
     {
-        [BurstCompile]
-        public struct ComponentData : IComponentData
-        {
-            [BurstCompile]
-            public readonly void Update()
-            {
-            }
-        }
-
-        private EntityManager m_EntityManager;
-        private Entity m_Entity;
-
-        protected override void OnEnable()
-        {
-            base.OnEnable();
-            m_EntityManager = EntityWorldHelper.GetOrCreateDefaultWorld().EntityManager;
-
-            m_Entity = m_EntityManager.CreateEntity(typeof(ComponentData), typeof(LocalTransform), typeof(LocalToWorld));
-            m_EntityManager.SetComponentData(m_Entity, LocalTransform.Identity);
-            m_EntityManager.SetName(m_Entity, new FixedString64Bytes(name));
-
-            var collider = SphereCollider.Create(new SphereGeometry
-            {
-                Center = float3.zero,
-                Radius = 0.5f
-            });
-
-            m_EntityManager.AddComponentData(m_Entity, new PhysicsCollider { Value = collider });
-
-            m_EntityManager.AddComponentData(m_Entity, PhysicsMass.CreateDynamic(collider.Value.MassProperties, 1f));
-            m_EntityManager.AddComponentData(m_Entity, PhysicsVelocity.Zero);
-            m_EntityManager.AddComponentData(m_Entity, new PhysicsDamping
-            {
-                Linear = 0.1f,
-                Angular = 0.05f
-            });
-            m_EntityManager.AddComponentData(m_Entity, new PhysicsGravityFactor { Value = 1f });
-            m_EntityManager.AddSharedComponent(m_Entity, new PhysicsWorldIndex { Value = 0 });
-        }
-
-        protected override void OnDisable()
-        {
-            base.OnDisable();
-            if (ApplicationMisc.IsInTearingDown() == false && m_Entity != Entity.Null)
-            {
-                m_EntityManager.DestroyEntity(m_Entity);
-                m_Entity = Entity.Null;
-            }
-        }
-
-        private void LateUpdate()
-        {
-            var lt = m_EntityManager.GetComponentData<LocalTransform>(m_Entity);
-            transform.SetLocalPositionAndRotation(lt.Position, lt.Rotation);
-        }
-
-        public Entity GetEntity() => m_Entity;
     }
 }
--- Runtime/Script/ActorFramework/CharacterMovement.cs
+++ Runtime/Script/ActorFramework/CharacterMovement.cs
@@ -1,44 +1,11 @@
-﻿#nullable enable
+#nullable enable
 
-using Unity.Burst;
-using Unity.Entities;
 using UnityEngine;
 
 namespace Ayla
 {
     [RequireComponent(typeof(Character))]
-    [BurstCompile]
     public class CharacterMovement : MonoBehaviour
     {
-        [BurstCompile]
-        public struct ComponentData : IComponentData
-        {
-            [BurstCompile]
-            public readonly void Update()
-            {
-            }
-        }
-
-        private EntityManager m_EntityManager;
-        private Entity m_Entity;
-
-        protected virtual void OnEnable()
-        {
-            var character = GetComponent<Character>();
-
-            m_EntityManager = EntityWorldHelper.GetOrCreateDefaultWorld().EntityManager;
-            m_Entity = character.GetEntity();
-
-            m_EntityManager.AddComponentData(m_Entity, new ComponentData());
-        }
-
-        protected virtual void OnDisable()
-        {
-            if (ApplicationMisc.IsInTearingDown() == false && m_Entity != Entity.Null)
-            {
-                m_EntityManager.RemoveComponent<ComponentData>(m_Entity);
-                m_Entity = Entity.Null;
-            }
-        }
     }
 }
